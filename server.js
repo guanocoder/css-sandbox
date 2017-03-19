6eaad2274b4cb1622229f355cf8a883aa03f5447
guanocoder <spy.white@mail.ru>
2017-03-19 13:49:32 +0300

    server param parser & support for PUT & POST so that client ngResource can save() and $update()

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require('path');
 const npmRequest = require('request');
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 const ipsumUrl = 'http://loripsum.net/api/3/medium/plaintext';
 let chicks = require("./chicks.json");
 
 // implement service to work with ngResource: https://www.sitepoint.com/creating-crud-app-minutes-angulars-resource/
+
+// serves ngResource.query() request
 app.get('/api/chicks', (request, response) => {
     let promises = [];
     chicks.forEach((chick) => {
@@ -30,6 +35,32 @@ app.get('/api/chicks', (request, response) => {
     });
 });
 
+// serves ngResource.save() request
+app.post('/api/chicks', (request, response) => {
+    let newChick = {
+        name: request.body.name,
+        fullName: request.body.fullName,
+        img: request.body.img,
+        description: request.body.description,
+        url: request.body.url
+    };
+    let chickAlreadyExists = false;
+
+    chicks.forEach((chick) => {
+        if(chick.name.toLowerCase() == newChick.name.toLowerCase())
+            chickAlreadyExists = true;
+    })
+
+    if(chickAlreadyExists)
+        response.statusCode = 500;
+    else {
+        chicks.unshift(newChick);
+        response.statusCode = 201;
+    }
+    response.end();
+});
+
+// serves ngResource.get('/api/chicks/:name') request
 app.get('/api/chicks/*', (request, response) => {
     let name = request.url.replace('/api/chicks/', '');
     let chickFound = false;
@@ -62,6 +93,30 @@ app.get('/api/chicks/*', (request, response) => {
         response.statusCode = 404;
         response.end();
     }
+});
+
+// serves ngResource.update('/api/chicks/:name') request
+app.put('/api/chicks/:name', (request, response) => {
+    let name = request.params.name;
+    let updatedChick = {
+        name: request.body.name,
+        fullName: request.body.fullName,
+        img: request.body.img,
+        description: request.body.description,
+        url: request.body.url
+    };    
+    let chickFound = false;
+    for(let i = 0; i < chicks.length; i++) {
+        if(chicks[i].name.toLowerCase() == name.toLowerCase()) {
+            chickFound = true;
+            chicks[i] = updatedChick;
+            response.statusCode = 204;
+        }
+    }
+    if(!chickFound) {
+        response.statusCode = 404;
+    }
+    response.end();
 });
 
 app.use(express.static('.'));
