28af76c93a6967633750a580053be0d5658ca7e1
guanocoder <spy.white@mail.ru>
2017-03-19 16:44:52 +0300

    CREATE and UPDATE on Angular side using $ngResource

--- js/app.js
+++ js/app.js
@@ -6,7 +6,15 @@ chicklistApp.config(function($routeProvider) {
     $routeProvider
     .when('/', {
         templateUrl: 'pages/chicklist.html',
-        controller: 'chickController'
+        controller: 'listController'
+    })
+    .when('/create', {
+        templateUrl: 'pages/chick-create.html',
+        controller: 'createController'
+    })
+    .when('/update', {
+        templateUrl: 'pages/chick-update.html',
+        controller: 'updateController'
     });
 })
 
@@ -19,9 +27,40 @@ chicklistApp.service('chickService', ['$resource', function($resource) {
     })
 }]);
 
-// controller
-chicklistApp.controller('chickController', ['$scope', 'chickService', function($scope, chickService) {
+
+// controllers
+chicklistApp.controller('listController', ['$scope', '$location', 'chickService', function($scope, $location, chickService) {
+
+
     chickService.query(function(data) {
         $scope.chicks = data;
     });
+
+    $scope.createChick = function() {
+        $location.path('create');
+    }
+
+    $scope.updateChick = function(name) {
+        $location.path('update').search({name: name});
+    }
+}]);
+
+chicklistApp.controller('createController', ['$scope', '$location', 'chickService', function($scope, $location, chickService) {
+    $scope.chick = new chickService();
+
+    $scope.chickCreate = function() {
+        $scope.chick.$save(function() {
+            $location.path('/'); // goto chick list when done
+        });
+    }
+}]);
+
+chicklistApp.controller('updateController', ['$scope', '$routeParams', '$location', 'chickService', function($scope, $routeParams, $location, chickService) {
+    $scope.chick = chickService.get({name: $routeParams.name});
+
+    $scope.chickUpdate = function() {
+        $scope.chick.$update(function() {
+            $location.path('/'); // goto chick list when done
+        });
+    }
 }]);
