822e4ff684df340e069fe279ec3948d4ef3d5bb5
guanocoder <spy.white@mail.ru>
2017-05-13 13:20:16 +0300

    gulp-livereload setup for watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const liveReload = require('gulp-livereload');
 
 gulp.task('sass', () => {
     return gulp.src("./scss/index.scss")
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./www/build/css'));
+        .pipe(gulp.dest('./www/build/css'))
+        .pipe(liveReload());
 })
 
 gulp.task('sass:watch', () => {
+    require('./server.js');
+    liveReload.listen();
     gulp.watch('./scss/**/*.scss', ['sass']);
 })
